38e57b56dffd14fa724c796cbc9d0468054e165c
Luan Paniagua <luanvictor.castro11@gmail.com>
2025-02-17 08:53:06 -0300

    Update index.php

--- ex004/index.php
+++ ex004/index.php
@@ -18,6 +18,20 @@ while ($estoque > 0) {
 
 echo "Estoque esgotado.\n";
 
+//Inicializa o estoque com 100 
+$estoque = 100;
+
+//Enquanto houver unidades no estoques
+while ($estoque > 0){
+    //Venda uma unidade por vez
+    $estoque--;
+    //Exibe a quantidade de unidades vendidas e o estoque restante
+    echo "Uma unidade ventida. Restam $estoque unidades no estoque. <br>";
+}
+
+
+echo "O estoque foi zerado!";
+
 ?>
 
 </body>
